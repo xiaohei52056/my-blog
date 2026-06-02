bff2f4262d4ead501a1eb94d73ee262a5afad2a8
xiaohei52056 <2662542568@qq.com>
2026-06-02 21:05:55 +0800

    添加Docker配置并打包

--- blog-frontend/src/api/request.js
+++ blog-frontend/src/api/request.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 import { markTokenInvalid } from '../utils/auth'
 
 const service = axios.create({
-  baseURL: 'http://8.148.6.228/api',
+  baseURL: 'http://8.148.6.228:8081/api',
   timeout: 10000
 })
 
